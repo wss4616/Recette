2bd4a311781eebd942666e6f23691dd9401c5708
wss4616 <wasimoto_4999@yahoo.co.jp>
2022-06-07 14:15:27 +0900

    ユーザー機能の実装とフロントの調整

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,10 @@
 class ApplicationController < ActionController::Base
   #before_action :authenticate_user!
   before_action :configure_permitted_parameters, if: :devise_controller?
+
+  def create
+  end
+  
   
   private
   def configure_permitted_parameters
